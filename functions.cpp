fc10aba461ccf07278e4a5968336987b238478a2
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2019-11-06 13:10:34 -0700

    updated krusal's to use cp-algorithm's disjoint sets

--- functions.cpp
+++ functions.cpp
@@ -12,54 +12,30 @@ struct mstEdge
 
 };
 
-vector<mstEdge> kruskal(vector<pair<double, double> > points)
+vector<mstEdge> kruskal(priority_queue<mstEdge> q, int verticies)
 {
-    priority_queue<mstEdge> q;
-    int verticies = points.size();
-    vector< set< int > > vertSets(verticies);
-    unordered_map<int, int> setLoc;
+	parent.resize(verticies, 0);
+	sizeArr.resize(verticies, 0);
+
+	for(int i = 0; i < verticies; i++)
+	{
+		make_set(i);
+	}
     vector<mstEdge> mst;
     mstEdge temp;
     int index1, index2;
-    
-    //create vector of sets with one vertex per set
-    //ex: {{0}, {1}, {2}, {3}}
-    for (int i = 0; i < verticies; i++)
-    {
-        vertSets[i].insert(i);
-        setLoc[i] = i;
-    }
-    
-    //calculate and push all edges into queue
-    for (int i = 0; i < points.size(); i++)
-    {
-        for (int j = i + 1; j < points.size(); j++)
-        {
-            temp.distance = sqrt((points[j].first - points[i].first) *
-                (points[j].first - points[i].first) + (points[j].second - points[i].second)
-                * (points[j].second - points[i].second));
-            temp.src = j;
-            temp.dest = i;
-            q.push(temp);
-        }
-    }
-  
-    //starting at shortest edge distance, if edge is across different sets, combine them
-    //also add edge to mst, when mst is complete return it
     while (mst.size() != verticies - 1)
     {
         temp = q.top();
         q.pop();
-        index1 = setLoc[temp.src];
-        index2 = setLoc[temp.dest];        
+        index1 = find_set(temp.src);
+        index2 = find_set(temp.dest);
+        
         if (index1 != index2)
         {
             mst.push_back(temp);
-            for (set<int>::iterator i = vertSets[index2].begin(); i != vertSets[index2].end(); i++)
-            {
-                vertSets[index1].insert((*i));
-                setLoc[*i] = index1;
-            }
+            union_sets(index1, index2);
+            //vertSets.erase(vertSets.begin() + index2);
         }
     }
 
