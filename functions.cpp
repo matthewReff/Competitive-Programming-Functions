c59580f3cac00a4d32b371c59bbfa817f82259f9
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2019-11-07 10:35:51 -0700

    added LIS

--- functions.cpp
+++ functions.cpp
@@ -281,7 +281,8 @@ struct BipartiteMatcher {
     }
 };
 
-	    
+
+// segment tree re-stolen from coach
 struct SegmentTree {
     vector<ll> treeSum, treeMax, lazy;
     ll n, root, size;
@@ -357,3 +358,42 @@ struct SegmentTree {
         return max(queryMax(2*node, start, mid, l, r), queryMax(2*node+1, mid+1, end, l, r));
     }
 };
+
+
+//LIS returning a vector containing the indecies of all elements in the LIS
+template <class T>
+vector<int> LIS(vector<T>& v) {
+    if(v.size() == 0) return {};
+    vector<int> p(v.size(), -1);
+    vector<int> t(v.size(), 0);
+
+    int lis = 1;
+    for(int i = 1; i < v.size(); i++) {
+        if(v[i] <= v[t[0]]) {
+            t[0] = i;
+        }
+        else if(v[i] > v[t[lis - 1]]) {
+            p[i] = t[lis - 1];
+            t[lis++] = i;
+        }
+        else {
+            int l = -1;
+            int r = lis - 1;
+            while(r - l > 1) {
+                int m = l + (r - l) / 2;
+                if(v[t[m]] >= v[i]) r = m;
+                else l = m;
+            }
+            p[i] = t[r - 1];
+            t[r] = i;
+        }
+    }
+
+    vector<int> ans;
+    for(int i = t[lis - 1]; i >= 0; i = p[i]) {
+        ans.push_back(i);
+    }
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
